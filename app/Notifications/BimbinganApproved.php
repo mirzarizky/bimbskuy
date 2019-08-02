1ebd4a176436d6a255f2a7d47238399daaebba9b
Mirza Rizky Maulana <mirzarizky@gmail.com>
2019-08-03 02:46:10 +0700

    bimbingan approved

---
+++ app/Notifications/BimbinganApproved.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\OneSignal\OneSignalButton;
+use NotificationChannels\OneSignal\OneSignalChannel;
+use NotificationChannels\OneSignal\OneSignalMessage;
+use NotificationChannels\OneSignal\OneSignalWebButton;
+
+class BimbinganApproved extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [
+            OneSignalChannel::class,
+            'database'
+        ];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    public function toOneSignal($notifiable)
+    {
+        return OneSignalMessage::create()
+            ->subject("Your {$notifiable->service} account was approved!")
+            ->body("Click here to see details.")
+            ->url('http://onesignal.com')
+            ->button(
+                OneSignalButton::create('link-1')
+                    ->text('Click here')
+                    ->icon('https://upload.wikimedia.org/wikipedia/commons/4/4f/Laravel_logo.png')
+                    ->url('http://laravel.com')
+            );
+    }
+}
